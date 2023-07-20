e8632d389bf6063ebb7f80ba9cb2d220998a5a7b
scloudmani <139200294+scloudmani@users.noreply.github.com>
2023-07-20 16:02:11 +0530

    Update afterinsatll.sh

--- afterinsatll.sh
+++ afterinsatll.sh
@@ -2,16 +2,10 @@
 
 # Change directory to /var/www/anyhow
 cd /var/www/anyhow
-
 # Run the commands inside /var/www/anyhow
-composer update
-
-php bin/magento maintenance:enable
-
-php bin/magento setup:upgrade
-
-php bin/magento setup:di:compile
-
-php bin/magento setup:static-content:deploy -f
-
-php bin/magento maintenance:disable
+sudo composer update
+sudo php bin/magento maintenance:enable
+sudo php bin/magento setup:upgrade
+sudo php bin/magento setup:di:compile
+sudo php bin/magento setup:static-content:deploy -f
+sudo php bin/magento maintenance:disable

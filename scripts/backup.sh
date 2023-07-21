66da1b31912148f80d8cb4c10fb9ac6c1089b73e
scloudmani <139200294+scloudmani@users.noreply.github.com>
2023-07-21 10:21:55 +0530

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-# Replace file1 with the new version
-sudo cp -f "auth.json.sample" /var/www/anyhow/auth.json.sample
+
+# Create a backup folder if it doesn't exist using sudo
+sudo mkdir -p "/var/www/anyhow_backup"
+
+# Move the specified files to the backup folder using sudo
+sudo mv /var/www/anyhow/auth.json.sample "/var/www/anyhow_backup/"
+sudo mv /var/www/anyhow/composer.json "/var/www/anyhow_backup/"
+sudo mv /var/www/anyhow/composer.lock "/var/www/anyhow_backup/"
+sudo mv /var/www/anyhow/nginx.conf.sample "/var/www/anyhow_backup/"
+
+# Note: The deployment process will add new versions of these files to /var/www/anyhow/
